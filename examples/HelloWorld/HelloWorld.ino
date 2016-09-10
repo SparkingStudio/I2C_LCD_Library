36925ba7578b77d5f0adf6563d39082fd7373987
JoneySui <joney.s@foxmail.com>
2016-09-10 11:58:22 +0800

    edit code

--- examples/HelloWorld/HelloWorld.ino
+++ examples/HelloWorld/HelloWorld.ino
@@ -9,24 +9,17 @@ uint8_t I2C_LCD_ADDRESS = 0x51; //Device address configuration, the default valu
 
 void setup(void)
 {
-    //Wire.begin();         //I2C controller initialization.
-    pinMode(13, OUTPUT);
+    Wire.begin();         //I2C controller initialization.
 }
 
 void loop(void)
 {
-    //LCD.CleanAll(WHITE);    //Clean the screen with black or white.
+    LCD.CleanAll(WHITE);    //Clean the screen with black or white.
     delay(1000);            //Delay for 1s.
 
     //8*16 font size��auto new line��black character on white back ground.
-    //LCD.FontModeConf(Font_6x8, FM_ANL_AAA, BLACK_BAC); 
-    //LCD.DispStringAt("Hello World!", 0, 10);    //Display "Hello World!" on coordinate of (0, 10).
+    LCD.FontModeConf(Font_6x8, FM_ANL_AAA, BLACK_BAC); 
+    LCD.DispStringAt("Hello World!", 0, 10);    //Display "Hello World!" on coordinate of (0, 10).
 
-    while(1)
-    {
-      digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(200);              // wait for a second
-      } //Wait for ever. 
+    while(1);//Wait for ever. 
 }
